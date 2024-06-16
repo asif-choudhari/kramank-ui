b422200b5178d9ed7876c2691ae519845928b421
asif-choudhari <asifahmed5@outlook.com>
2024-06-16 15:26:00 +0530

    dasboard home refinement

--- src/area/dashboard/home/home.tsx
+++ src/area/dashboard/home/home.tsx
@@ -12,13 +12,20 @@ import {
 } from "@/components/ui/table";
 
 const pieChartOtherProps = {
-  legend: { hidden: true },
+  margin: { right: 5, bottom: 100 },
+  legend: {
+    hidden: false,
+    position: {
+      horizontal: "middle",
+      vertical: "bottom",
+    },
+  },
 };
 
 const getChartWidth = (): number => {
-  if (window.innerWidth < 640) return 200; // sm
-  if (window.innerWidth < 1024) return 250; // md
-  return 300; // lg and above
+  if (window.innerWidth < 640) return 200;
+  if (window.innerWidth < 1024) return 250;
+  return 300;
 };
 
 const data = [
@@ -45,7 +52,7 @@ function Home() {
 
   return (
     <div className="h-svw w-svw py-4 pl-4 pr-8 grid gap-5 grid-cols-1 lg:grid-cols-2 lg:grid-rows-5 bg-slate-100 overflow-y-auto hide-scrollbar">
-      <div className="w-auto p-5 shadow-all-sides rounded-xl flex flex-col items-center bg-white lg:row-span-3">
+      <div className="w-auto py-5 shadow-all-sides rounded-xl flex flex-col items-center bg-white lg:row-span-3">
         <span className="pt-2 pb-8 text-xl font-bold">Consumption</span>
         <PieChart
           series={[
@@ -59,12 +66,11 @@ function Home() {
             },
           ]}
           width={getChartWidth()}
-          height={250}
-          margin={{ right: 5 }}
+          height={300}
           {...pieChartOtherProps}
         />
       </div>
-      <div className="w-auto h-full py-8 shadow-all-sides rounded-2xl flex flex-col sm:flex-row items-center justify-evenly shadow-all bg-white lg:row-span-2">
+      <div className="w-auto h-full py-10 px-5 shadow-all-sides rounded-2xl flex flex-col sm:flex-row items-center justify-evenly shadow-all bg-white lg:row-span-2">
         <div className="flex flex-col items-center pb-16 sm:pb-0">
           <img src={branchesImage} alt="branches" className="h-24 w-24" />
           <span className="pt-5 font-bold">No. of Branches</span>
@@ -76,7 +82,7 @@ function Home() {
           <span className="pt-2">4 (States)</span>
         </div>
       </div>
-      <div className="w-auto h-full py-8 shadow-all-sides rounded-2xl flex flex-col items-center justify-center xl:flex-row sm:justify-evenly shadow-all bg-white lg:col-start-1 lg:row-start-4 lg:row-end-6">
+      <div className="w-auto h-full py-10 px-5 shadow-all-sides rounded-2xl flex flex-col items-center justify-center xl:flex-row sm:justify-evenly shadow-all bg-white lg:col-start-1 lg:row-start-4 lg:row-end-6">
         <div className="flex flex-col items-center pb-12 lg:pb-0 text-lg">
           <span className="font-bold">Average consumption</span>
           <span>Rs. 20,00,000</span>
@@ -86,13 +92,9 @@ function Home() {
           <span>Rs. 5,00,00,000</span>
         </div>
       </div>
-      <div className="w-auto h-full p-10 shadow-all-sides rounded-2xl flex flex-col shadow-all bg-white lg:col-start-2 lg:row-start-3 lg:row-end-6">
-        <div className="pb-8">
-          <img
-            src={consumptionImage}
-            alt="branches"
-            className="h-12 w-12 inline-block"
-          />
+      <div className="w-auto h-full py-10 px-5 shadow-all-sides rounded-2xl flex flex-col shadow-all bg-white lg:col-start-2 lg:row-start-3 lg:row-end-6">
+        <div className="pb-8 flex items-center">
+          <img src={consumptionImage} alt="branches" className="h-12 w-12" />
           <span className="px-5 font-bold text-xl">Rank of Consumption</span>
         </div>
         <Table>
